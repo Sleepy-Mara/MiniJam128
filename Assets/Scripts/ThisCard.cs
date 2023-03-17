52b82e55ebf74da2f543826ea4e4ef6fc98c4336
Morcroc <morcrocthepro@hotmail.com>
2023-03-17 10:51:18 -0300

    Cartas maomeno

---
+++ Assets/Scripts/ThisCard.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ThisCard : MonoBehaviour
+{
+    public Cards card;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI attackText;
+    [SerializeField] private TextMeshProUGUI lifeText;
+    [SerializeField] private TextMeshProUGUI manaCostText;
+    [SerializeField] private TextMeshProUGUI healthCostText;
+    [SerializeField] private Image image;
+    private void Awake()
+    {
+        nameText.text = card.name;
+        image.sprite = card.sprite;
+        attackText.text = card.attack.ToString();
+        lifeText.text = card.life.ToString();
+        manaCostText.text = card.manaCost.ToString();
+        healthCostText.text = card.healthCost.ToString();
+    }
+}
