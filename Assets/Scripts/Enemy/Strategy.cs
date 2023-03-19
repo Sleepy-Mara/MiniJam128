fa49a89d2493ee11b8e466bdaaf83f8f123ed74f
Morcroc <morcrocthepro@hotmail.com>
2023-03-19 01:08:27 -0300

    porsiacaso

--- Assets/Scripts/Enemy/Strategy.cs
+++ Assets/Scripts/Enemy/Strategy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Strategy")]
 public class Strategy : ScriptableObject
 {
     public CardPlaceing[] turns;
