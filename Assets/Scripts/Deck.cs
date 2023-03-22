a70656adb9613d9f4047b81149414ddcc32323b3
Morcroc <morcrocthepro@hotmail.com>
2023-03-22 18:34:20 -0300

    ladron

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Deck : MonoBehaviour
 {
     private Draw draw;
+    private Animator animator;
     private void Start()
     {
         draw = FindObjectOfType<Draw>();
+        animator = GetComponentInParent<Animator>();
     }
     private void OnMouseDown()
+    {
+        if (draw.canDraw)
+            animator.SetTrigger("Activate");
+    }
+    public void DrawedCard()
     {
         draw.PlayerDraw();
     }
