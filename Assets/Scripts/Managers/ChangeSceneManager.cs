873ff01548b774202fd2d5d1f4a539ed9e071fc5
tomasagamenon <tomas.agamenon@gmail.com>
2023-03-26 09:22:46 -0300

    Monkeys

--- Assets/Scripts/Managers/ChangeSceneManager.cs
+++ Assets/Scripts/Managers/ChangeSceneManager.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneManager : MonoBehaviour
 {
+    [HideInInspector]
+    private static bool win;
+    public bool Win;
+    private static ChangeSceneManager instance;
+    [SerializeField]
+    private GameObject buttonMonkey;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+        if (win && buttonMonkey != null)
+            buttonMonkey.SetActive(true);
+    }
     public void ChangeScene(string scene)
     {
+        win = Win;
         Time.timeScale = 1;
         SceneManager.LoadScene(scene);
     }
