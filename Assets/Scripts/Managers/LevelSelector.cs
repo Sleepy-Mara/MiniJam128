667bfe0ed2fbf815b0abfa117a93b93242823c75
Tomas Fernandez <tomas.agamenon@gmail.com>
2023-08-20 20:16:35 -0300

    LevelSelectorFix

--- Assets/Scripts/Managers/LevelSelector.cs
+++ Assets/Scripts/Managers/LevelSelector.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelSelector : MonoBehaviour
 {
-    private SaveWithJson json;
-    private int unlockedLevels;
     [SerializeField] private List<GameObject> lockLevel;
     [SerializeField] private string sceneToGo;
-    private void Awake()
+    private void Start()
     {
-        json = FindObjectOfType<SaveWithJson>();
-        unlockedLevels = json.SaveData.currentUnlockedLevels;
-        for(int i = 0; i <= unlockedLevels; i++)
+        SaveWithJson json = FindObjectOfType<SaveWithJson>();
+        int unlockedLevels = json.SaveData.currentUnlockedLevels;
+        for (int i = 0; i <= unlockedLevels; i++)
             lockLevel[i].SetActive(true);
     }
     public void SelectedLevel(int selectedLevel)
